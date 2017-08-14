bfac44ffdc9b6a9dc9f91934f0f158c6e9f0dc1b
bagdenia <bagdenia@gmail.com>
2017-08-14 23:46:17 +0300

    Preliminary state

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,19 @@
 class UsersController < ApplicationController
+  before_action :load_user, only: :show
+  respond_to :html
+
   def index
   end
 
   def create
-
+    respond_with(@user = User.create(user_params))
   end
 
   private
+  def load_user
+    @user = User.find(params[:id])
+  end
+
   def user_params
     params.require(:user).permit(:name, :latitude, :longitude)
   end
